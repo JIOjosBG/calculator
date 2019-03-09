d19e06c90b61e812937bac86d818941198685fdd
JIOjosBG <jjjooosssiiifff@gmail.com>
2019-03-09 23:57:15 +0100

    added the rest

---
+++ calc.py
@@ -0,0 +1,15 @@
+import basicarithmetic as basicarithmetic
+
+class Calculator:
+    def execute(self, operation, a, b):
+        if operation == '+':
+            basicarithmetic.add(a, b)
+        elif operation == '-':
+            basicarithmetic.subtract(a, b)
+        elif operation == '*':
+            basicarithmetic.multiply(a, b)
+        elif(operation == '/'):
+            basicarithmetic.divide(a, b)
+
+c = Calculator()
+c.execute('+', 3, 4)
